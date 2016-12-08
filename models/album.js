ffb9ca51904b15d5e13d08490f63e0e4574cde6e
kevinbloomquist <rkevinbloom@gmail.com>
2016-12-07 23:02:15 -0700

    end of day: working on sprint 1 step 4

--- models/album.js
+++ models/album.js
@@ -1,2 +1,15 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+
+var AlbumSchema = new Schema({
+"id":String,
+"artistName": String,
+"name": String,
+"releaseDate": String,
+"genres": [String]
+
+});
+
+var Album = mongoose.model('Album',AlbumSchema);
+module.exports = Album;
